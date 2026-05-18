6da7516e7b9955affa0572e450ce21902da80c2f
Andrey Yurchuk <r1430@tut.by>
2026-05-18 06:40:07 +0300

    refactor: convert App shell to functional component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,13 @@
-import { Component, type ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppErrorBoundary } from '../components/AppErrorBoundary/index.ts';
 import { AppRoutes } from '../routes/AppRoutes';
 
-export default class App extends Component {
-  render(): ReactNode {
-    return (
-      <BrowserRouter>
-        <AppErrorBoundary>
-          <AppRoutes />
-        </AppErrorBoundary>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppErrorBoundary>
+        <AppRoutes />
+      </AppErrorBoundary>
+    </BrowserRouter>
+  );
 }
