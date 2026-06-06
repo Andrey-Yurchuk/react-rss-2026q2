37e0a62fea704a50a0e9db08c6e3fa899ef7bd21
Andrey Yurchuk <r1430@tut.by>
2026-06-06 12:02:22 +0300

    feat: add uncontrolled profile form

--- src/components/ProfileForms/index.ts
+++ src/components/ProfileForms/index.ts
@@ -19,3 +19,5 @@ export type {
 } from './ProfileFormFields.tsx';
 export { PasswordStrengthIndicator } from './PasswordStrengthIndicator.tsx';
 export type { PasswordStrengthIndicatorProps } from './PasswordStrengthIndicator.tsx';
+export { UncontrolledProfileForm } from './UncontrolledProfileForm.tsx';
+export type { UncontrolledProfileFormProps } from './UncontrolledProfileForm.tsx';
